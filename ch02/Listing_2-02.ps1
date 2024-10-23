33e6e96cb9ab37a4fe59919764eba16e5597a618
David Stevenson <drsgis@rit.edu>
2024-10-23 10:24:33 -0400

    Listing 2.2 Running the application

---
+++ ch02/Listing_2-02.ps1
@@ -0,0 +1,14 @@
+# Listing 2.2 Running the application
+
+dotnet run --project WebApplication1\WebApplication1.csproj
+
+# PS C:\Users\drsgis\drs\ASP-NET-Core-Razor-Pages-In-Action\ch02> .\Listing_2-02.ps1
+# Building...
+# info: Microsoft.Hosting.Lifetime[14]
+#       Now listening on: http://localhost:5153
+# info: Microsoft.Hosting.Lifetime[0]
+#       Application started. Press Ctrl+C to shut down.
+# info: Microsoft.Hosting.Lifetime[0]
+#       Hosting environment: Development
+# info: Microsoft.Hosting.Lifetime[0]
+#       Content root path: C:\Users\drsgis\drs\ASP-NET-Core-Razor-Pages-In-Action\ch02\WebApplication1
