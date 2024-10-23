c2c477f82441749fe1d1782f267f0c2c04a67bc3
David Stevenson <drsgis@rit.edu>
2024-10-23 10:20:53 -0400

    Listing 2.1 Creating Razor Pages application using the CLI

---
+++ ch02/Listing_2-01.ps1
@@ -0,0 +1,20 @@
+# Listing 2.1 Creating Razor Pages application using the CLI
+
+dotnet new sln
+dotnet new webapp -o WebApplication1
+dotnet sln add WebApplication1\WebApplication1.csproj
+
+# PS C:\Users\drsgis\drs\ASP-NET-Core-Razor-Pages-In-Action\ch02> .\Listing_2-01.ps1
+# The template "Solution File" was created successfully.
+
+# The template "ASP.NET Core Web App (Razor Pages)" was created successfully.
+# This template contains technologies from parties other than Microsoft, see https://aka.ms/aspnetcore/8.0-third-party-notices for details.
+
+# Processing post-creation actions...
+# Restoring C:\Users\drsgis\drs\ASP-NET-Core-Razor-Pages-In-Action\ch02\WebApplication1\WebApplication1.csproj:
+#   Determining projects to restore...
+#   Restored C:\Users\drsgis\drs\ASP-NET-Core-Razor-Pages-In-Action\ch02\WebApplication1\WebApplication1.csproj (in 70 ms).
+# Restore succeeded.
+
+
+# Project `WebApplication1\WebApplication1.csproj` added to the solution.
