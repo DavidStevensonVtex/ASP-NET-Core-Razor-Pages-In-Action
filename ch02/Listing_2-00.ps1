0ba63bbb230f793e8438748c4cb50fea53e4d4a9
David Stevenson <drsgis@rit.edu>
2024-10-23 10:12:30 -0400

    Chapter 2: Building your first application - Listing 2.0 Lising SDKs and checking for Trusted HTTPS Certificate

---
+++ ch02/Listing_2-00.ps1
@@ -0,0 +1,9 @@
+# Listing 2.0 Lising SDKs and checking for Trusted HTTPS Certificate
+
+dotnet --list-sdks
+
+dotnet dev-certs https --trust
+
+# 8.0.204 [C:\Program Files\dotnet\sdk]
+# Trusting the HTTPS development certificate was requested. A confirmation prompt will be displayed if the certificate was not previously trusted. Click yes on the prompt to trust the certificate.
+# Successfully trusted the existing HTTPS certificate.
