0b0011a29d72ba0372ea5b78456aebda305a81da
David Stevenson <drsgis@rit.edu>
2024-10-23 10:34:28 -0400

    Listing 2.2b Create a Razor Page using the Command Line Interface

---
+++ ch02/Listing_2-02b.ps1
@@ -0,0 +1,7 @@
+# Listing 2.2b Create a Razor Page using the Command Line Interface
+
+Set-Location .\WebApplication1
+dotnet new page -n Welcome -o Pages 
+
+# PS C:\Users\drsgis\drs\ASP-NET-Core-Razor-Pages-In-Action\ch02> .\Listing_2-02b.ps1
+# The template "Razor Page" was created successfully.
