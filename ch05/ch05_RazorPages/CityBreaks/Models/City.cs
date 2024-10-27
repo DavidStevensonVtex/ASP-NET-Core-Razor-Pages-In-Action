6e03a0eebb979e0e611116a257e4777831b98070
David Stevenson <drstevenson1958@yahoo.com>
2024-10-27 19:34:48 -0400

    Listing 5.13 The City class in the City.cs file in the CityBreaks/Models folder

---
+++ ch05/ch05_RazorPages/CityBreaks/Models/City.cs
@@ -0,0 +1,10 @@
+﻿// Listing 5.13 The City class in the City.cs file in the CityBreaks/Models folder
+
+namespace CityBreaks.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
