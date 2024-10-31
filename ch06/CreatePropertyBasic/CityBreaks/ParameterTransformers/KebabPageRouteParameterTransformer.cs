effcf8346700c8a5b557705eb6d9e31219ca05fa
David Stevenson <drstevenson1958@yahoo.com>
2024-10-31 15:30:04 -0400

    Add routing convention: KebabPageRouteParameterTransformer, use lower case URLs
    i.e. /PropertyManager/Create beocmes /property-manager/create

---
+++ ch06/CreatePropertyBasic/CityBreaks/ParameterTransformers/KebabPageRouteParameterTransformer.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CityBreaks.ParameterTransformers
+{
+    public class KebabPageRouteParameterTransformer : IOutboundParameterTransformer
+    {
+        public string? TransformOutbound(object? value)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+#pragma warning disable CS8604
+            return Regex.Replace(value.ToString(), "([a-z])([A-Z])", "$1-$2");
+        }
+    }
+}
--- ch06/CreatePropertyBasic/CityBreaks/Program.cs
+++ ch06/CreatePropertyBasic/CityBreaks/Program.cs
@@ -1,7 +1,17 @@
+using CityBreaks.ParameterTransformers;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options => {
+    options.Conventions.Add(new PageRouteTransformerConvention(new KebabPageRouteParameterTransformer()));
+});
+
+builder.Services.Configure<RouteOptions>(options =>
+{
+    options.LowercaseUrls = true;
+});
 
 var app = builder.Build();
 
